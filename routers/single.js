b96027e27040613efc711659fb071d1933413237
hambarcumyannarek <narmenia878@gmail.com>
2024-03-15 15:58:47 +0400

    new version

--- routers/single.js
+++ routers/single.js
@@ -7,9 +7,7 @@ router.get('/:id', async (req, res) => {
         const [getProductInfo] = await pool.query('select products.id as productId, userId, productName, homeAddress, bedRooms, bathRooms, parkingPlace, location, description, price, images, date, property, category, users.name as userName, users.email as userEmail from products inner join users on users.id = userId where products.id=?', [req.params.id]);
         const state = {...getProductInfo[0], images: getProductInfo[0].images.split(' ')};
         console.log(state);
-        if(req.cookies.JWT_SIGN) {
-            var userObj = await JWT.verify(req.cookies.JWT_SIGN, process.env.JWT_SECRET)
-        }
+        var userObj = req.user;
         res.render('single.ejs', {getProductInfo: state, thisMy: userObj ? state.userId === userObj.id ? true : false : false})
     } catch(err) {
         res.send('not defined')
